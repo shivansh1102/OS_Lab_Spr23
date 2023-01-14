b8eb93f4d6c1a1fa2babcdf4815be6d067923ea0
Shivansh Shukla <shivanshshukla60@gmail.com>
2023-01-15 00:20:38 +0530

    Word count 19, can improve

---
+++ Assign1/q1_shivansh.sh
@@ -0,0 +1,3 @@
+findGCD(){ if(($2==0));then gcd=$1;else findGCD $2 $(($1%$2));fi;}
+solve(){ rev=0;num=$1;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;findGCD $lcm $rev;lcm=$((lcm*rev/gcd));}
+lcm=1;while read line;do solve $line;done < lcm.txt;echo $lcm
