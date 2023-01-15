82efcf04a3a2d085b39354ac3756661c8c957b94
Shivansh Shukla <shivanshshukla60@gmail.com>
2023-01-15 12:38:55 +0530

    Reduced WC to 18

--- Assign1/q2_shivansh.sh
+++ Assign1/q2_shivansh.sh
@@ -1,2 +1,1 @@
-substringCheck(){ flag=0;str=$1;while read fruit;do if echo $str|grep -iqF $fruit;then flag=1;fi;done<fruits.txt;((flag==0))&&echo YES||echo NO;}
-while read str;do ((${#str}>=5&&${#str}<=20))&&[[ $str =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]]&&substringCheck $str||echo NO;done<username_sample.txt;
+while read str;do ((${#str}>=5&&${#str}<=20))&&[[ $str =~ ^[a-zA-Z][a-zA-Z0-9]*[0-9][a-zA-Z0-9]*$ ]]&&flag=0&&while read fruit;do if echo $str|grep -iqF $fruit;then flag=1;fi;done<fruits.txt&&((flag==0))&&echo YES||echo NO;done<usernames.txt;
