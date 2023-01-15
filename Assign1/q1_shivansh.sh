610ff47ac134716c0668686f79eea2109b5007ad
Shivansh Shukla <shivanshshukla60@gmail.com>
2023-01-15 11:45:19 +0530

    WC=7git add q1_shivansh.sh !

--- Assign1/q1_shivansh.sh
+++ Assign1/q1_shivansh.sh
@@ -1,2 +1,1 @@
-findGCD(){ (($2==0))&&gcd=$1||findGCD $2 $(($1%$2));}
-lcm=1;while read num;do rev=0;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;findGCD $lcm $rev;lcm=$((lcm/gcd*rev));done<lcm.txt;echo $lcm
+lcm=1;while read num;do rev=0;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;a=$lcm;b=$rev;while((b>0));do temp=$b;b=$((a%b));a=$temp;done;gcd=a;lcm=$((lcm/gcd*rev));done<lcm.txt;echo $lcm
