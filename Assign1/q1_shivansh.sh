68bbec0f14ff1ac5add4188d11433c371f28d8ab
Shivansh Shukla <shivanshshukla60@gmail.com>
2023-01-15 11:05:38 +0530

    Removed integer overflow & WC reduced to 12

--- Assign1/q1_shivansh.sh
+++ Assign1/q1_shivansh.sh
@@ -1,3 +1,2 @@
-findGCD(){ if(($2==0));then gcd=$1;else findGCD $2 $(($1%$2));fi;}
-solve(){ rev=0;num=$1;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;findGCD $lcm $rev;lcm=$((lcm*rev/gcd));}
-lcm=1;while read line;do solve $line;done<lcm.txt;echo $lcm
+findGCD(){ (($2==0))&&gcd=$1||findGCD $2 $(($1%$2));}
+lcm=1;while read num;do rev=0;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;findGCD $lcm $rev;lcm=$((lcm/gcd*rev));done<lcm.txt;echo $lcm
