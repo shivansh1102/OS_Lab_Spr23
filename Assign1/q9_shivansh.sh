35d309b769714839c3bcb337212c9bf23a04e548
Shivansh Shukla <shivanshshukla60@gmail.com>
2023-01-16 17:27:29 +0530

    WC=22: Compressed earlier one

--- Assign1/q9_shivansh.sh
+++ Assign1/q9_shivansh.sh
@@ -1,4 +1,1 @@
-declare -A deptCount majorCount
-while read -a line;do deptCount[${line[1]}]=$((${deptCount[${line[1]}]}+1));majorCount[${line[0]}]=$((${majorCount[${line[0]}]}+1));done<q3_sample.txt
-for dept in ${!deptCount[@]};do echo $dept ${deptCount[$dept]}>>q9_output.txt;done;sort -k2nr q9_output.txt;
-cntSingleMajor=0;for student in ${!majorCount[@]};do ((${majorCount[$student]}>=2))&&echo $student||cntSingleMajor=$((cntSingleMajor+1));done;echo $cntSingleMajor;
+declare -A deptCount majorCount;while read -a line;do deptCount[${line[1]}]=$((${deptCount[${line[1]}]}+1));majorCount[${line[0]}]=$((${majorCount[${line[0]}]}+1));done<q3_sample.txt;for dept in ${!deptCount[@]};do echo $dept ${deptCount[$dept]}>>q9_output.txt;done;sort -k2nr q9_output.txt;cntSingleMajor=0;for student in ${!majorCount[@]};do ((${majorCount[$student]}>=2))&&echo $student||cntSingleMajor=$((cntSingleMajor+1));done;echo $cntSingleMajor;
