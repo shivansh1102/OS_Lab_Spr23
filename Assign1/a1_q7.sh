d2ac2b65dc9a41b3255f2d57bd9a5bfa3c08f4dd
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-01-16 22:21:41 +0530

    Add files via upload
    question 7 -> final word count 25

--- Assign1/a1_q7.sh
+++ Assign1/a1_q7.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-files=`find $1 -type f -name "*.txt"`;mkdir -p $2;for x in {A..Z};do for file in $files;do grep "^$x.*\|^${x,,}" $file>>"$2/$x.txt";done;sort -o "$2/$x.txt"{,};done
+files=`find $1 -type f -name "*.txt"`
+
+mkdir -p $2
+
+for x in {A..Z};do
+	for file in $files;do
+		grep "^$x.*\|^${x,,}" $file >> "$2/$x.txt";done;sort -o "$2/$x.txt"{,};done
