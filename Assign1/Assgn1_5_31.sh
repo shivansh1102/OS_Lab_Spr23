535cdff07d12ff64a476c065c526d8ccd054d42a
likhnic <87529764+likhnic@users.noreply.github.com>
2023-01-24 22:40:50 +0530

    Correction Ultra Pro Max S T Plus

--- Assign1/Assgn1_5_31.sh
+++ Assign1/Assgn1_5_31.sh
@@ -2,65 +2,69 @@ files=`find $1 -type f -name "*.py"`
 for file in $files
 do
     echo $file
-    printf "\n****************** Single Line Comments ****************** \n"
     gawk '
+    BEGIN{
+        s_start=0;
+        d_start=0;
+    }
     {
-        if (/^[^"]*("[^"]*")*[^"]*#/)
-        {
-            if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
-            {
-                print NR ": " $0 ; next;
-            } next;
-        }
-        if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
-        {
+        if(d_start==0){
+            if(s_start==0){
+                if(/^[ \t]*["]["]["].*["]["]["][ \t]*$/){
+                    print NR ": " $0 ;
+                    next;
+                }
+                if(/^[ \t]*[\047][\047][\047].*[\047][\047][\047][ \t]*$/){
+                    print NR ": " $0 ;
+                    next;
+                }
+                if(/^[ \t]*["]["]["]/){
+                    print NR ": " $0 ;
+                    d_start=1;
+                    next;
+                }
+                if(/^[ \t]*[\047][\047][\047]/){
+                    print NR ": " $0 ;
+                    s_start=1;
+                    next;
+                }
+            }
+            else{
+                print $0
+                if(/[\047][\047][\047][ \t]*/){
+                    s_start=0;
+                    next;
+                }
+                next;
+
+            }
             if (/^[^"]*("[^"]*")*[^"]*#/)
             {
-                print NR ": " $0 ; next;
-            } next;
-        }
-    }' $file
-    printf "\n"
-
-    printf "\n****************** Line numbers of start of multiline comments ****************** \n"
-    gawk '
-    BEGIN{
-        d_start = 0;
-    }
-    {
-        if (/^[ \t]*"""/ && d_start == 0)
-        {
-            d_start = 1;
-            print NR;
-            if (/^[ \t]*"""(["][ ]|[""][ ]|[^"])*"""/){
-                d_start = 0;
+                if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
+                {
+                    print NR ": " $0 ; 
+                    next;
+                } 
+                next;
             }
-            next;
-        }
-        if(d_start==1 && /.*"""[ \t]*$/){
-            d_start=0;
-            next;
-        }
-    }
-    ' $file
-    gawk '
-    BEGIN{
-        d_start = 0;
-    }
-    {
-        if (/^[ \t]*\047\047\047/ && d_start == 0)
-        {
-            d_start = 1;
-            print NR;
-            if (/^[ \t]*\047\047\047([\047][ ]|[\047\047][ ]|[^\047])*\047\047\047/){
-                d_start = 0;
+            if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
+            {
+                if (/^[^"]*("[^"]*")*[^"]*#/)
+                {
+                    print NR ": " $0 ; 
+                    next;
+                } 
+                next;
             }
-            next;
         }
-        if(d_start==1 && /.*\047\047\047[ \t]*$/){
-            d_start=0;
-            next;
+        else{
+            print $0;
+            if(/["]["]["][ \t]*$/){
+                d_start=0;
+                next;
+            }
         }
+        
     }
     ' $file
     printf "\n"
