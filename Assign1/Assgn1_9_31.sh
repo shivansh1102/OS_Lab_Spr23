2073e4b5558f50d232f5582ef0ee671df62cfc02
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-01-25 01:21:21 +0530

    WC=97

--- Assign1/Assgn1_9_31.sh
+++ Assign1/Assgn1_9_31.sh
@@ -1,5 +1,5 @@
-gawk 'function key_to_val(i1,v1,i2,v2){
-    if(v1==v2&&i1<i2||v1>v2)return -1
+gawk 'function key_to_val (i1,v1,i2,v2) {
+    if(v1==v2&&i1<i2||v1>v2) return -1
     return 1
 }
 {
@@ -7,7 +7,6 @@ gawk 'function key_to_val(i1,v1,i2,v2){
     majors[$2]++
 }
 END{
-    count=0
     PROCINFO["sorted_in"] = "key_to_val"
     for(i in majors) print(i,majors[i])
     for(i in students){
