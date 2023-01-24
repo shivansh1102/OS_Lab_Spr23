e5ca82113aac71edad17d45e81e2c2c88c79796f
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-01-24 15:01:22 +0530

    correctly working

--- Assign1/Assgn1_5_31.sh
+++ Assign1/Assgn1_5_31.sh
@@ -3,7 +3,11 @@ for file in $files
 do 
     printf "\n$file"
     printf "\n****************** Single Line Comments ****************** \n"
-    grep -n '\.*[#]\.*' $file
+    (cat $file; echo) | while IFS= read -r line; do
+        if(grep -q '^[^"]*\("[^"]*"\)*[^"]*#' <<<"$line" && grep -q "^[^']*\('[^']*'\)*[^']*#" <<<"$line");then
+            printf "%s\n" "$line"
+        fi
+    done
     printf "\n****************** Line numbers of start of multiline comments ****************** \n"
     grep -ne '^[ \t]*"""\.*' -e "^[ \t]*'''\.*" $file | awk -F: '{print $1}'
     printf "\n\n"
