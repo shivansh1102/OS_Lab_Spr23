21f54aacc6d9b6baf66db27861df1d909c0d5ec0
likhnic <87529764+likhnic@users.noreply.github.com>
2023-01-24 20:14:06 +0530

    WC 58

--- Assign1/Assgn1_1_31.sh
+++ Assign1/Assgn1_1_31.sh
@@ -2,14 +2,14 @@ numbers=`cat $1 | rev`
 lcm=1
 for num in $numbers
 do 
-    a=lcm
+    a=$lcm
     b=$num
-    while(($b>0))
+    while((10#$b>0))
     do 
         temp=$b
-        b=$(($a%$b))
+        b=$((10#$a%10#$b))
         a=$temp
     done
-    lcm=$((lcm*($num/$a)))
+    lcm=$((lcm*(10#$num/10#$a)))
 done
 echo $lcm
