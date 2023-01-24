26079fd6fb86c41138d5d673425f4f040bdf65df
likhnic <87529764+likhnic@users.noreply.github.com>
2023-01-24 23:37:12 +0530

    WC 21

--- Assign1/Assgn1_4_31.sh
+++ Assign1/Assgn1_4_31.sh
@@ -1,5 +1,1 @@
-(cat $1; echo)|while IFS= read -r line
-do
-    grep -qw "$2" <<< "$line"&&curr=${line,,}&&curr=`sed 's/[a-z][^a-z]*[a-z]\?/\u\0/g' <<< "$curr"`||curr="$line"
-    printf "%s\n" "$curr"
-done
+sed "/$2/{s/.*/\L&/;s/[a-z][^a-z]*[a-z]\?/\u\0/g;}" $1
