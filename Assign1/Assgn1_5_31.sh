728b723add90aee278987ba6eeb9e31efce4d55b
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-01-24 18:08:55 +0530

    corrected ultra pro max plus

--- Assign1/Assgn1_5_31.sh
+++ Assign1/Assgn1_5_31.sh
@@ -1,14 +1,67 @@
 files=`find $1 -type f -name "*.py"`
 for file in $files
-do 
-    printf "\n$file"
+do
+    echo $file
     printf "\n****************** Single Line Comments ****************** \n"
-    (cat $file; echo) | while IFS= read -r line; do
-        if(grep -q '^[^"]*\("[^"]*"\)*[^"]*#' <<<"$line" && grep -q "^[^']*\('[^']*'\)*[^']*#" <<<"$line");then
-            printf "%s\n" "$line"
-        fi
-    done
+    gawk '
+    {
+        if (/^[^"]*("[^"]*")*[^"]*#/)
+        {
+            if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
+            {
+                print NR ": " $0 ; next;
+            } next;
+        }
+        if (/^[^\047]*(\047[^\047]*\047)*[^\047]*#/)
+        {
+            if (/^[^"]*("[^"]*")*[^"]*#/)
+            {
+                print NR ": " $0 ; next;
+            } next;
+        }
+    }' $file
+    printf "\n"
+
     printf "\n****************** Line numbers of start of multiline comments ****************** \n"
-    grep -ne '^[ \t]*"""\.*' -e "^[ \t]*'''\.*" $file | awk -F: '{print $1}'
-    printf "\n\n"
+    gawk '
+    BEGIN{
+        d_start = 0;
+    }
+    {
+        if (/^[ \t]*"""/ && d_start == 0)
+        {
+            d_start = 1;
+            print NR;
+            if (/^[ \t]*"""(["][ ]|[""][ ]|[^"])*"""/){
+                d_start = 0;
+            }
+            next;
+        }
+        if(d_start==1 && /.*"""[ \t]*$/){
+            d_start=0;
+            next;
+        }
+    }
+    ' $file
+    gawk '
+    BEGIN{
+        d_start = 0;
+    }
+    {
+        if (/^[ \t]*\047\047\047/ && d_start == 0)
+        {
+            d_start = 1;
+            print NR;
+            if (/^[ \t]*\047\047\047([\047][ ]|[\047\047][ ]|[^\047])*\047\047\047/){
+                d_start = 0;
+            }
+            next;
+        }
+        if(d_start==1 && /.*\047\047\047[ \t]*$/){
+            d_start=0;
+            next;
+        }
+    }
+    ' $file
+    printf "\n"
 done
