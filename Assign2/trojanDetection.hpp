81cc924bddaa82a948cfad45df8c9a4a7e8a6181
likhnic <87529764+likhnic@users.noreply.github.com>
2023-02-09 14:07:54 +0530

    Vaguely Added

--- Assign2/trojanDetection.hpp
+++ Assign2/trojanDetection.hpp
@@ -1,13 +1,22 @@
+#ifndef __TROJANDETECT_HPP
+#define __TROJANDETECT_HPP
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// maps pid of a process to its cpu time
-map<pid_t, long long>cpuTime;      
-// maps pid of a process to count of children in subtree rooted at this node having cpu utilisation >= 95% of cpu util of suspected user process
-map<pid_t, long long>cntTrojanSubtree;      
 
 pid_t findPPid(pid_t pid);
 vector<pid_t> findAllChildren(pid_t pid);
 long long findCPUtime(pid_t pid);
 long long findCntTrojanSubtree(pid_t parProcess,const pid_t &childDone, int depth);
 pid_t detectTrojan(pid_t suspectedUser);
+void malwareDetect(int pid);
+
+vector<pid_t> findAllChildren_(pid_t pid);
+long long findCPUtime_(pid_t pid);
+void getAllParents(pid_t suspectedPid);
+long long getCPUJiffles();
+pid_t getTrojan(pid_t suspectedPid);
+void fasterMalware(int pid, bool flag);
+
+#endif
