628cc2ce6ed71bf0005958ee541aa1c28faad94a
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-02-09 16:54:59 +0530

    Update stringProcessing.hpp

--- Assign2/stringProcessing.hpp
+++ Assign2/stringProcessing.hpp
@@ -15,20 +15,13 @@ using namespace std;
 // function to trim a string
 string trim(string &s);
 
-// function to split a string into tokens
-vector<vector<string> >getAllTokens(string &command);
-
 // function to get all pipes
 vector<string> getAllPipes(string &command);
 
 // function to handle * and ?
 void handleRegex(vector<string>&cmd, string &temp);
 
-// function to get all tokens
-vector<string> getAllCmds(string &segmentCmd);
-
 // function to get all vectored tokens
-vector<vector<string> > getAllVectoredTokens(string &command);
-
+vector<vector<string>> getAllVectoredTokens(string &command);
 
 #endif
