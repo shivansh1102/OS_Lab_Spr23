21d2e06e72641859a91a625ca7ea5b8144fb80bc
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-02-24 16:11:23 +0530

    done

---
+++ Assign3/producer.hpp
@@ -0,0 +1,20 @@
+#ifndef __PRODUCER_HPP
+#define __PRODUCER_HPP
+
+#include <set>
+#include "main.hpp"
+
+extern struct nodeData *nodes;
+extern struct edgeData* edges;
+
+extern char* bufNode;  // pointer to shared memory storing nodes
+extern char* bufEdge;  // pointer to shared memory storing edges
+
+extern int currNodes; // to store count of current nodes
+extern int currEdges; // to store count of current edges
+
+void solveProducer();
+void allocateNewEdges(const int &newEdges);
+void allocateNewNodes(const int &newNodes);
+
+#endif
