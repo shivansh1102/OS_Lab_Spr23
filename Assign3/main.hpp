f483e90652f74998e18d300c820f0b5c327fab24
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-02-24 15:15:16 +0530

    added degree in nodes

--- Assign3/main.hpp
+++ Assign3/main.hpp
@@ -11,15 +11,33 @@
 using namespace std;
 
 // REF : https://codeforces.com/blog/entry/67883
-// Using this graph representation.
+// Using this graph representation with a modification of storing degree of each node also
 struct edgeData
 {
     int to;
     int nxt;
+    edgeData()
+    {
+        to = -1;
+        nxt = -1;
+    }
 };
 
-int *head;
+struct nodeData
+{
+    int degree;
+    int head;
+    nodeData()
+    {
+        degree = 0;
+        head = -1;
+    }
+};
+nodeData *nodes;
 edgeData* edges;
+
+char* bufNode;  // pointer to shared memory storing nodes
+char* bufEdge;  // pointer to shared memory storing edges
 
 int currNodes; // to store count of current nodes
 int currEdges; // to store count of current edges
