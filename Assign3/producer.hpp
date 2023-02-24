a33747b20e19708ce48d9302d189c8cb0cb4324c
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-02-24 22:17:29 +0530

    currnodes & curredges added in shared memory

--- Assign3/producer.hpp
+++ Assign3/producer.hpp
@@ -10,8 +10,8 @@ extern struct edgeData* edges;
 extern char* bufNode;  // pointer to shared memory storing nodes
 extern char* bufEdge;  // pointer to shared memory storing edges
 
-extern int currNodes; // to store count of current nodes
-extern int currEdges; // to store count of current edges
+extern int *currNodes; // to store count of current nodes
+extern int *currEdges; // to store count of current edges
 
 void solveProducer();
 void allocateNewEdges(const int &newEdges);
