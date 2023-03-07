cee42bdb99e0cea98d481e084e07a455bdc561b9
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-03-08 04:31:59 +0530

    Update sns.cpp

--- Assign4/sns.cpp
+++ Assign4/sns.cpp
@@ -4,6 +4,9 @@ const int MAXNODES = 37700, MAXEDGES = 289003;
 Node* nodes = new Node[MAXNODES];
 queue<Action> updates;
 
+pthread_mutex_t mutexUpdateQueue, mutexFeedQueue;
+pthread_cond_t condUpdateQueue = PTHREAD_COND_INITIALIZER, condFeedQueue = PTHREAD_COND_INITIALIZER;
+
 Node::Node() : degree(0), typeFeed(rand()%2) {}
 
 Node::~Node()
@@ -40,19 +43,24 @@ void Node::updateMutualFriendsNeigh(int x)
     cout << x << ": " << cnt << endl;
 }
 
-Action::Action(int userID = 0, int actionID = 0, int actionType = 0) : user_id(userID), action_id(actionID), action_type(actionType)
+bool Action::operator < (const Action& other) const     // Writing "const" is a must here as internally priority_queue implementation needs it for const objects.
+{
+    return action_id < other.action_id;
+}
+
+Action::Action(int userID = 0, int actionID = 0, int globalID = 0, int actionType = 0) : user_id(userID), action_id(actionID), global_action_id(globalID), action_type(actionType)
 {
     time(&timestamp);
 }
 
 Action::~Action()
 {
-    cout << "Deleting action - user_id:" << user_id << " action_id:" << action_id << " action_type:" << action_type << " timestamp:" << timestamp << endl;
+    // cout << "Deleting action - user_id:" << user_id << " action_id:" << action_id << " action_type:" << action_type << " timestamp:" << timestamp << endl;
 }
 
 ofstream& operator << (ofstream& outFile, const Action& obj)
 {
-    outFile << "Action generated- user_id:" << obj.user_id << " action_id:" << obj.action_id << " action_type:" << obj.action_type << " timestamp:" << obj.timestamp;
+    outFile << "user_id:" << obj.user_id << " action_id:" << obj.action_id << " action_type:" << obj.action_type << " timestamp:" << obj.timestamp;
     return outFile;
 }
 
@@ -106,29 +114,42 @@ int main()
     //     cout << i << " " << j << " -> " << nodes[i].cntMutualFriends[j] << endl;
     // }
 
+    if(pthread_mutex_init(&mutexUpdateQueue, NULL) != 0)
+    {
+        cerr << "Error in initialising mutexUpdateQueue" << endl;
+        exit(1);
+    }
+
+    if(pthread_mutex_init(&mutexFeedQueue, NULL) != 0)
+    {
+        cerr << "Error in initialising mutexFeedQueue" << endl;
+        exit(1);
+    }
+
     pthread_t userSimTID, readPostTID[10], pushUpdTID[25];
     pthread_attr_t usattr, rpattr[10], pdattr[25];
     // Initialising thread attributes with default values
     pthread_attr_init(&usattr);
     // for(int i = 0; i < 10; i++)
     // pthread_attr_init(&rpattr[i]);
-    // for(int i = 0; i < 25; i++)
-    // pthread_attr_init(&pdattr[i]);
+    for(int i = 0; i < 25; i++)
+    pthread_attr_init(&pdattr[i]);
 
     // Creating threads
     pthread_create(&userSimTID, &usattr, userSimulator, nullptr);
     // for(int i = 0; i < 10; i++)
     // pthread_create(&readPostTID[i], &rpattr[i], readPosts, nullptr);
-    // for(int i = 0; i < 25; i++)
-    // pthread_create(&pushUpdTID[i], &pdattr[i], pushUpdates, nullptr);
+    for(int i = 0; i < 25; i++)
+    pthread_create(&pushUpdTID[i], &pdattr[i], pushUpdates, static_cast<void*>(&i));
 
     // // Waiting for threads    
     pthread_join(userSimTID, nullptr);
     // for(int i = 0; i < 10; i++)
     // pthread_join(readPostTID[i], nullptr);
-    // for(int i = 0; i < 25; i++)
-    // pthread_join(pushUpdTID[i], nullptr);
+    for(int i = 0; i < 25; i++)
+    pthread_join(pushUpdTID[i], nullptr);
 
+    pthread_mutex_destroy(&mutexUpdateQueue);
     delete[] nodes;
     return 0;
 }
