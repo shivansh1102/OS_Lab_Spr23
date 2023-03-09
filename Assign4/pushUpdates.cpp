c91a863fc2b09484cd2170b3a192af48f7d922d0
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-03-09 15:01:43 +0530

    Update pushUpdates.cpp

--- Assign4/pushUpdates.cpp
+++ Assign4/pushUpdates.cpp
@@ -2,9 +2,7 @@
 
 void pushOneToFeed(const Action &obj, ofstream& outFile)
 {
-    int _priority;
-    
-
+    int _priority, quIndex;
     for(int currNeigh : nodes[obj.user_id].neigh)
     {
         if(nodes[currNeigh].typeFeed)           // because by default, priority queue is a max heap and we want
@@ -12,14 +10,20 @@ void pushOneToFeed(const Action &obj, ofstream& outFile)
         else
         _priority = nodes[currNeigh].cntMutualFriends[obj.user_id];
 
-        pthread_mutex_lock(&mutexFeedQueue);
+        pthread_mutex_lock(&mutexFeedQueue[currNeigh]);
         nodes[currNeigh].feedQueue.push({_priority, obj});
-        pthread_cond_signal(&condFeedQueue);
-        pthread_mutex_unlock(&mutexFeedQueue);
+        pthread_cond_signal(&condFeedQueue[currNeigh]);
+        pthread_mutex_unlock(&mutexFeedQueue[currNeigh]);
 
         outFile << "Pushed action- ";
         outFile << obj;
         outFile << " to feedQueue of Node #" << currNeigh << endl;
+
+        quIndex = currNeigh/(MAXNODES/10);
+        pthread_mutex_lock(&mutexUpdNodeFeed[quIndex]);
+        updNodeFeed[quIndex].push(currNeigh);
+        pthread_cond_signal(&condUpdNodeFeed[quIndex]);
+        pthread_mutex_unlock(&mutexUpdNodeFeed[quIndex]);
     }                                
 }
 
