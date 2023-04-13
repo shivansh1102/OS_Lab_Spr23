fa94243197506675313a348a165f055c1bf2ad8b
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-04-13 13:07:00 +0530

    Add files via upload

--- Assign6/goodmalloc.cpp
+++ Assign6/goodmalloc.cpp
@@ -1,6 +1,13 @@
 #include "goodmalloc.hpp"
 
-FrameStack::FrameStack() : currIdx(0) {}
+Stack *stack;
+PageTable* ptable;
+
+
+FrameStack::FrameStack() : currIdx(0) 
+{
+    cout << "FrameStack created" << endl;
+}
 
 void FrameStack::push(const uint32_t& sp)
 {
@@ -10,6 +17,7 @@ void FrameStack::push(const uint32_t& sp)
         exit(1);
     }
     fptrs[currIdx++] = sp;
+    cout << "New frame created: frame pointer: " << sp << endl;
 }
 void FrameStack::pop()
 {
@@ -22,30 +30,40 @@ void FrameStack::pop()
 }
 uint32_t FrameStack::top()
 {
-    return fptrs[currIdx];
+    if(currIdx == 0)
+    {
+        cerr << "Empty frame stack" << endl;
+        exit(1);
+    }
+    return fptrs[currIdx-1];
 }
 
-Stack::Stack(uint32_t endOffset) : sp(endOffset-(uint32_t)sizeof(Stack)) {}
+Stack::Stack(uint32_t endOffset) : sp(endOffset-(uint32_t)sizeof(Stack)) 
+{
+    cout << "Stack created. stack pointer: " << sp << endl;
+}
 
-void Stack::newFuncBegin()
+void Stack::newFuncBeginUtil()
 {
     fstack.push(sp);
 }
-void Stack::FuncEnd()
+void Stack::FuncEndUtil()
 {
     fstack.pop();
 }
 void Stack::push(const stackEntry &entry)
 {
+    cout << "Old stack pointer: " << sp << endl;
     sp -= 2;
     char* temp = startBuffer+sp;
     *(reinterpret_cast<uint16_t*>(temp)) = entry.pageTableIdx;
     sp -= entry.listNameLen;
     temp = startBuffer+sp;
-    strcpy(temp, entry.listName);
+    strncpy(temp, entry.listName, entry.listNameLen);
     sp -= 1;
     temp = startBuffer+sp;
     *(reinterpret_cast<uint8_t*>(temp)) = entry.listNameLen;
+    cout << "New entry pushed into stack. New stack pointer: " << sp << endl;
 }
 stackEntry Stack::top()
 {
@@ -66,9 +84,96 @@ void Stack::pop()
     sp += 1 + listNameLen + 2;
 }
 
+void createMem(uint32_t bytes)
+{
+    startBuffer = new char[bytes];
+    totalBytesMalloced = bytes;
+    stack = new(startBuffer+bytes-sizeof(Stack)) Stack(bytes);
+    ptable = new(startBuffer) PageTable();
+    cout << "Memory Created!" << endl;
+}
+
+void createList(uint32_t size, char* listName, uint8_t listNameLen)
+{
+    size <<= 2;             // converting into bytes
+    uint16_t ptidx = ptable->allocateList(size);
+    if(ptidx == MAX_PAGE_TABLE_ENTRIES)
+    return;
+    printf("%s %d\n", listName, listNameLen);
+    stackEntry se;
+    se.listName = new char[listNameLen+1];      // for'\0'
+    strncpy(se.listName, listName, listNameLen);
+    se.listName[listNameLen] = '\0';
+    se.listNameLen = listNameLen+(uint8_t)1;
+    se.pageTableIdx = ptidx;
+    stack->push(se);
+    cout << "List created successfully" << endl;
+    delete se.listName;
+}
+
+void assignVal(char* listName, uint8_t listNameLen, uint32_t offset, uint32_t val)
+{
+    uint16_t ptidx = stack->findPTidxByName(listName, listNameLen);
+    if(ptidx == MAX_PAGE_TABLE_ENTRIES)
+    {
+        cerr << "No such list with given name exists" << endl;
+        exit(1);
+    }
+    ptable->assignValUtil(ptidx, offset, val);
+}
+
+uint32_t getVal(char* listName, uint8_t listNameLen, uint32_t offset)
+{
+    uint16_t ptidx = stack->findPTidxByName(listName, listNameLen);
+    if(ptidx == MAX_PAGE_TABLE_ENTRIES)
+    {
+        cerr << "No such list with given name exists" << endl;
+        exit(1);
+    }
+    return ptable->getValUtil(ptidx, offset);
+}
+
+void freeElem()
+{
+    uint16_t ptidx;
+    while((ptidx = stack->freeLastListCurrStack()) != MAX_PAGE_TABLE_ENTRIES)
+    ptable->freeList(ptidx);
+}
+
+void freeElem(char* listName, uint32_t listNameLen)
+{
+    uint16_t ptidx = stack->findPTidxByName(listName, listNameLen);
+    ptable->freeList(ptidx);
+}
+
+
+void testFunc()
+{
+
+}
+
+void newFuncBegin()
+{
+    stack->newFuncBeginUtil();
+}
+
+void FuncEnd()
+{
+    stack->FuncEndUtil();
+}
 
 int main()
 {
-    
+    cout << sizeof(PageTable)  << " " << sizeof(Stack) << endl;
+    const int SIZE = 1000*1000;
+    createMem(SIZE);
+    newFuncBegin();
+    createList(100, "arr", 3);
+    cout << "hi" << endl;
+    for(int i = 0; i < 100; i++)
+    assignVal("arr", 3, i, i+1);
+    for(int i = 99; i >= 0; i--)
+    cout << getVal("arr", 3, i) << endl;
+    FuncEnd();
     return 0;
 }
--- Assign6/goodmalloc.hpp
+++ Assign6/goodmalloc.hpp
@@ -5,11 +5,13 @@
 #include <cstring>
 using namespace std;
 
-
 const int MAX_FRAME_CNT = 10000;
-const int MAX_PAGE_TABLE_ENTRIES = 32768;
+const int MAX_PAGE_TABLE_ENTRIES = 16384;   // 2^15
 
-char* startBuffer;                           
+char* startBuffer;                      // to store malloced segment header        
+uint32_t startMem, endMem;              // to store offset of starting and ending portion of mem
+uint32_t totalBytesMalloced;
+
 
 struct stackEntry
 {
@@ -18,7 +20,7 @@ struct stackEntry
     uint8_t listNameLen;                // assuming name of list can fit in 8 bits 
     stackEntry()
     {
-        pageTableIdx = -1;
+        pageTableIdx = MAX_PAGE_TABLE_ENTRIES;
         listName = NULL;
         listNameLen = 0;
     }
@@ -42,11 +44,41 @@ class Stack
     FrameStack fstack;                      // stack of frame pointers
     public:
     Stack(uint32_t);
-    void newFuncBegin();                    // Save the current stack pointer as frame pointer in frame stack
-    void FuncEnd();                         // Pop frame pointer entry
+    void newFuncBeginUtil();                    // Save the current stack pointer as frame pointer in frame stack
+    void FuncEndUtil();                         // Pop frame pointer entry
     void push(const stackEntry &);          // Push stack entry 
     stackEntry top();                       // Returns top entry from stack
     void pop();                             // Pop stack entry
+    uint16_t findPTidxByName(char* listName, uint8_t listNameLen)
+    {
+        cout << fstack.top() << endl;
+        int currsp = sp;
+        char* temp = startBuffer+sp;
+        uint8_t lNLen;
+        while(currsp > fstack.top())
+        {
+            lNLen = *(reinterpret_cast<uint8_t*>(temp));
+            temp += 1;
+            if(listNameLen == lNLen && strncpy(temp, listName, listNameLen))
+            {
+                temp += lNLen;
+                cout << "Page table index found in current stack: " << *reinterpret_cast<uint16_t*>(temp) << endl;
+                return *reinterpret_cast<uint16_t*>(temp);
+            }
+            temp += lNLen + 2;
+            currsp += 1 + lNLen + 2;
+        }
+        return MAX_PAGE_TABLE_ENTRIES;
+    }
+    uint16_t freeLastListCurrStack()
+    {
+        if(sp == fstack.top())
+        return MAX_PAGE_TABLE_ENTRIES;
+        stackEntry se = top();
+        pop();
+        return se.pageTableIdx;
+    }
+    // friend class PageTable;
 };
 
 struct pageTableEntry
@@ -56,8 +88,8 @@ struct pageTableEntry
     u_int isValid : 1;                      // isValid = 0 => chunk is free
     pageTableEntry()
     {
-        headOffset = -1;
-        nextFreeIdx = -1;
+        headOffset = 0;
+        nextFreeIdx = MAX_PAGE_TABLE_ENTRIES;
         isValid = 0;
     }
 };  
@@ -72,31 +104,116 @@ class PageTable
     PageTable() : firstFreeIdx(0), lastFreeIdx(0), currSize(0)
     {
         ptearr[currSize].headOffset = sizeof(PageTable);
-        ptearr[currSize].nextFreeIdx = -1;
+        ptearr[currSize].nextFreeIdx = MAX_PAGE_TABLE_ENTRIES;
         ptearr[currSize].isValid = 0;
+        startMem = sizeof(PageTable);
+        endMem = totalBytesMalloced - sizeof(Stack);
+        *reinterpret_cast<uint32_t*>(startBuffer+startMem) = endMem-startMem+1;
+        *reinterpret_cast<uint32_t*>(startBuffer+endMem-4000) = endMem-startMem+1;
         ++currSize;
+        cout << "PageTable created" << endl;
     }
     void insert(const pageTableEntry& pte)
     {
+        if(currSize == MAX_PAGE_TABLE_ENTRIES)
+        {
+            cerr << "Page table full" << endl;
+            exit(1);
+        }
         ptearr[currSize] = pte;
         ptearr[lastFreeIdx].nextFreeIdx = currSize;
         lastFreeIdx = currSize;
+        cout << "New page table entry at " << currSize << " index" << endl;
         ++currSize;
     }
-    uint16_t findFreeBlockIdx(uint8_t bytes)
+    uint16_t findFreeBlockIdx(uint32_t bytes)
     {
-        uint16_t idx = -1;
-        for(uint16_t currIdx = firstFreeIdx; currIdx <= lastFreeIdx; currIdx = ptearr[currIdx].nextFreeIdx)
+        for(uint16_t currIdx = firstFreeIdx; currIdx != MAX_PAGE_TABLE_ENTRIES; currIdx = ptearr[currIdx].nextFreeIdx)
         {
-            if(*reinterpret_cast<uint32_t*>(ptearr[currIdx].headOffset) >= bytes + 8)
+            if((*reinterpret_cast<uint32_t*>(startBuffer+ptearr[currIdx].headOffset)) >= bytes && (!ptearr[currIdx].isValid))    // as higher 31 bits store the actual length
             {
-                idx = currIdx;
+                return currIdx;
                 break;
             }
         }
+        return MAX_PAGE_TABLE_ENTRIES;
+    }
+    uint16_t allocateList(uint32_t bytes)
+    {
+        uint16_t idx = findFreeBlockIdx(bytes+8);           // because we need to store header & tail of implicit list block
+        if(idx == MAX_PAGE_TABLE_ENTRIES)
+        {
+            cout << "No free block exists!\n" << endl;
+            return idx;
+        }
+        cout << "Free Block found at head offset " << ptearr[idx].headOffset << endl;
+        uint32_t len = *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset);
+        if(len > bytes+8)
+        {
+            pageTableEntry newent;
+            newent.headOffset = ptearr[idx].headOffset+bytes+8;
+            newent.isValid = 0;
+            newent.nextFreeIdx = MAX_PAGE_TABLE_ENTRIES;
+            insert(newent);
+            *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset+len-4) = len-bytes-8;
+            *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset+bytes+8) = len-bytes-8;
+            *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset+bytes+4) = bytes+8;
+            *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset) = bytes+8;
+        }
+        ptearr[idx].isValid = 1;
+        cout << "List allocated" << endl;
         return idx;
     }
-    
+    void assignValUtil(uint16_t idx, uint32_t listIndex, uint32_t val)
+    {
+        uint32_t offset = listIndex<<2;         // offset in bytes
+        uint32_t len = *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset);
+        if(len-8 <= offset)
+        {
+            cerr << "Invalid offset" << endl;
+            exit(1);
+        }
+        *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset+offset) = val;
+        cout << "Value assigned at memory offset: " << ptearr[idx].headOffset+offset << endl;
+    }
+    uint32_t getValUtil(uint16_t idx, uint32_t listIndex)
+    {
+        uint32_t offset = listIndex<<2;
+        if(ptearr[idx].headOffset-8 <= offset)
+        {
+            cerr << "Invalid offset" << endl;
+            exit(1);
+        }
+        return *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset+offset);
+    }
+    uint16_t findPTidxByHeadOffset(uint32_t headoffset)     // req by coalesce
+    {
+        for(int i = 0; i < currSize; i++)
+        {
+            if(ptearr[i].headOffset == headoffset)
+            return i;
+        }
+        return MAX_PAGE_TABLE_ENTRIES;
+    }
+    // void coalesce(uint16_t idx)
+    // {
+    //     // check for front
+    //     uint32_t len = *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset);
+    //     uint16_t nextBlockIdx = findPTidxByHeadOffset(ptearr[idx].headOffset+len);
+    //     if(nextBlockIdx == -1)      // if no more block exists next to this block
+    //     return;
+    //     ptearr[nextBlockIdx].isValid = 1;       // making it NOT FREE
+    //     *reinterpret_cast<uint32_t*>(startBuffer+ptearr[idx].headOffset) = 
+    // }
+    void freeList(uint16_t idx)
+    {
+        if(ptearr[idx].isValid == 0)        // if it was already freed earlier
+        return;
+        ptearr[idx].isValid = 0;
+        ptearr[idx].nextFreeIdx = MAX_PAGE_TABLE_ENTRIES;
+        ptearr[lastFreeIdx].nextFreeIdx = idx;
+        lastFreeIdx = idx;
+    }
 };
 
 
