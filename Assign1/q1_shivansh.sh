9e67b75eedf5114010bbc9462a77ab443fba772c
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-04-25 11:05:42 +0530

    Delete q1_shivansh.sh

--- Assign1/q1_shivansh.sh
+++
@@ -1,1 +0,0 @@
-lcm=1;while read num;do rev=0;while((num>0));do rev=$((rev*10+num%10));num=$((num/10));done;a=$lcm;b=$rev;while((b>0));do temp=$b;b=$((a%b));a=$temp;done;gcd=a;lcm=$((lcm/gcd*rev));done<lcm_sample.txt;echo $lcm
